cb24b22beccd9ec5ca6691bd4783748b6fd5d7c4
Pavan Mamindla <pavanmamindla1@gmail.com>
2025-09-11 19:32:50 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
       <nav className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-between">
           <div className="text-2xl font-bold text-gray-900">
-            Welcome😃<span className="text-blue-600"></span>
+          Welcome😃<span className="text-blue-600"></span>
           </div>
 
           {/* Desktop Menu */}

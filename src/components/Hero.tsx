5b546787f139111897a60d0372657f3b8aa92f86
Pavan Mamindla <pavanmamindla1@gmail.com>
2025-09-11 20:41:23 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -48,7 +48,7 @@ const Hero = () => {
             
             <a 
               href="https://github.com/Pavan01Mamindla/portifolio/blob/main/Resume.docx" 
-              download="Mamindla-Pavan-Resume.pdf"
+              download="Resume.docx"
               className="flex items-center space-x-2 border-2 border-gray-300 text-gray-700 px-8 py-4 rounded-full font-semibold hover:border-blue-600 hover:text-blue-600 transition-all duration-200"
             >
               <Download size={20} />
